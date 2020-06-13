8c45398df587313dbd6b8845fe349ff59fbbb1fd
Jinei <jinei.pf@gmail.com>
2020-06-13 15:14:46 +0900

    パラメータからの日付操作

--- app/Http/Controllers/timelineController.php
+++ app/Http/Controllers/timelineController.php
@@ -9,15 +9,29 @@ class timelineController extends Controller
 {
     public function index($checkdate="")
     {   
-        $lastday = Carbon::now()->endOfMonth()->day;
-        $date = array();
+        // パラメータがない場合(現在日付の日付データ取得)
+        if($checkdate == "") {
+            $checkdt = Carbon::now();
+
+        // パラメータがある場合（指定した日付の日付データ取得）
+        } else {
+            $checkyear = substr($checkdate, 0,4);
+            $checkmonth = substr($checkdate,-1);
+            $checkdt = Carbon::create($checkyear,$checkmonth);
+        }
+
+        $lastday = $checkdt->endOfMonth()->day; //末日の取得
+        $date = array(); //日付データを入れる配列
+
+        // 1日分ずつ配列に日付データを入れる
         for($i = 1; $i <= $lastday; $i++){
-            $dt = Carbon::now();
-            $dt = Carbon::create($dt->year,$dt->month,$i);
+            $dt = Carbon::create($checkdt->year,$checkdt->month,$i);
             array_push($date,$dt);
         }
-        $weekday = ['日', '月', '火', '水', '木', '金', '土'];
-        $employee = ["長野","金谷","益田"];
-        return view('timeline.index',compact('employee','lastday','date','weekday','checkdate'));
+
+        $weekday = ['日', '月', '火', '水', '木', '金', '土']; //曜日変換用
+        $employee = ["長野","金谷","益田"]; //従業員のデータ
+        
+        return view('timeline.index',compact('employee','lastday','date','weekday','checkdt'));
     }
 }
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -17,7 +17,7 @@
   <nav class="navbar navbar-inverse">
     <div class="container-fluid">
       <div class="navbar-header">
-        <a class="navbar-brand" href="#">ホルトガーデン</a>
+        <a class="navbar-brand" href="#">{{$checkdt}}</a>
       </div>
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">勤務表</a></li>
