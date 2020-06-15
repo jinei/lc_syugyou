b66bb9602b38f50f1989d0b27252df71aeaf82ac
Jinei <jinei.pf@gmail.com>
2020-06-15 22:03:38 +0900

    データベースの変更(employee -> users)

--- app/Http/Controllers/timelineController.php
+++ app/Http/Controllers/timelineController.php
@@ -30,18 +30,18 @@ class timelineController extends Controller
 
         // 従業員IDと従業員名の取得
         $usercollection = collect(['userid','name','starttime','endtime','day','workingid']); //コレクションの定義
-        $employee = DB::select('select * from employee');//従業員のデータ
+        $employee = DB::select('select * from users');//従業員のデータ
         
         for($i = 0;$i < count($employee);$i++) {
             // 勤務時間をユーザーごとに取得
-            $working = DB::select('select * from working where year='.$checkdt->year.' and month='.$checkdt->month.' and userid='.$employee[$i]->userid);
+            $working = DB::select('select * from working where year='.$checkdt->year.' and month='.$checkdt->month.' and userid='.$employee[$i]->id);
             $starttime = array_column($working, 'starttime'); //勤務開始時間
             $endtime = array_column($working, 'endtime'); //勤務終了時間
             $workingid = array_column($working, 'id');
             $day = array_column($working, 'day'); //勤務日
 
             // 勤務時間をユーザーごとにコレクションに格納
-            $data[$employee[$i]->userid] = $usercollection->combine([$employee[$i]->userid,$employee[$i]->name,$starttime,$endtime,$day,$workingid]);
+            $data[$employee[$i]->id] = $usercollection->combine([$employee[$i]->id,$employee[$i]->name,$starttime,$endtime,$day,$workingid]);
         }
         
         $weekday = ['日', '月', '火', '水', '木', '金', '土']; //曜日変換用
