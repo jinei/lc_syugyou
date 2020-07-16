a3161c3e893a6ee674ec5e403cab9f2e8e8e801a
Jinei <jinei.pf@gmail.com>
2020-07-16 21:43:22 +0900

    標準のログインをカスタマイズ

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,53 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <title>勤務表アプリ</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="{{ asset('login.css') }}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<body>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <div class="text-center" style="padding:50px 0">
+	    <div class="logo">LOGIN</div>
+	    <div class="login-form-1">
+        
+            <form method="POST" action="{{ route('login') }}">
+            @csrf
+			    <div class="login-form-main-message"></div>
+			    <div class="main-login-form">
+				    <div class="login-group">
+					    <div class="form-group">
+						    <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" id="lg_username" name="email" placeholder="EMAIL">
                         </div>
+                        @error('email')
+                            <span style="color:#980000;">{{$message}}</span>
+                        @enderror
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+					    <div class="form-group">
+						    <label for="lg_password" class="sr-only">Password</label>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="lg_password" name="password" placeholder="PASSWORD">
                         </div>
+                        @error('password')
+                            <span style="color:#980000;">{{$message}}</span>
+                        @enderror
+                    
+                    </div>
+                    <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
+			    </div>
+            </form>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('timeline/{checkyear}/{checkmonth}/{checkuserid}','timelineController@index'); //勤務表 初期表示
-Route::post('timeline/{checkyear}/{checkmonth}/{checkuserid}','timelineController@database'); //勤務表 db操作
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('timeline/{checkyear}/{checkmonth}/{checkuserid}','timelineController@index'); //勤務表 初期表示
+    Route::post('timeline/{checkyear}/{checkmonth}/{checkuserid}','timelineController@database'); //勤務表 db操作
 
-Route::get('','LoginController@index'); //ログイン画面 初期表示
-Route::post('','AuthController@login'); //ログイン画面 ログイン機能
-Route::get('account','AccountController@index'); //アカウント作成画面 初期表示
-Route::post('create','AuthController@create'); //アカウント作成画面 作成機能
-Route::post('delete','AuthController@delete'); //アカウント作成画面 削除機能
-Route::post('logout','AuthController@logout'); //ログアウト機能
+    Route::get('','LoginController@index'); //ログイン画面 初期表示
+    Route::post('','AuthController@login'); //ログイン画面 ログイン機能
+    Route::get('account','AccountController@index'); //アカウント作成画面 初期表示
+    Route::post('create','AuthController@create'); //アカウント作成画面 作成機能
+    Route::post('delete','AuthController@delete'); //アカウント作成画面 削除機能
+    Route::post('logout','AuthController@logout'); //ログアウト機能
+});
+
+Auth::routes();
+    Route::get('/home', 'HomeController@index')->name('home');
