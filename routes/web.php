2ae434b3ac1b1b5af770cbcf9e4cbfa5314be874
Jinei <jinei.pf@gmail.com>
2020-07-17 23:04:46 +0900

    リファクタリング

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| 認証必須画面
+|--------------------------------------------------------------------------
+*/
 Route::group(['middleware' => 'auth'], function () {
     Route::get('timeline/{checkyear}/{checkmonth}/{checkuserid}','timelineController@index'); //勤務表 初期表示
     Route::post('timeline/{checkyear}/{checkmonth}/{checkuserid}','timelineController@database'); //勤務表 db操作
-    
     Route::get('account','AccountController@index'); //アカウント作成画面 初期表示
     Route::post('create','AuthController@create'); //アカウント作成画面 作成機能
     Route::post('delete','AuthController@delete'); //アカウント作成画面 削除機能
     Route::post('logout','AuthController@logout'); //ログアウト機能
 });
 
+
+/*
+|--------------------------------------------------------------------------
+| 認証不要画面
+|--------------------------------------------------------------------------
+*/
 Auth::routes();
-    Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
