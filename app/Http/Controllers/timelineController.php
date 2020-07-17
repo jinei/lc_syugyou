0198a0871a59dbbae3bc2395f7b1be79039edf0a
Jinei <jinei.pf@gmail.com>
2020-07-18 03:02:20 +0900

    勤務情報の取得 モデル

--- app/Http/Controllers/timelineController.php
+++ app/Http/Controllers/timelineController.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Auth;
+use App\Models\User;
+use App\Models\Working;
+
 
 class timelineController extends Controller
 {
@@ -31,35 +34,33 @@ class timelineController extends Controller
         return $this->getData($param_date[0],$param_date[1],$param_user);
     }
 
-    //------------------------------------------------------/
-    // ------------- 勤務情報・日付情報の取得 -------------
-    //------------------------------------------------------/
+
+    // 出勤情報・日付情報の取得
     public function getData($checkyear,$checkmonth,$checkuserid)
     {
         Carbon::useMonthsOverflow(false); //日付の加算方法を変更
 
-        // 日付データ取得
         $checkdt = Carbon::create($checkyear,$checkmonth);
         $lastday = $checkdt->endOfMonth()->day; //末日の取得
         $date = array(); //日付データを入れる配列
 
-        // 日付取得
         for($i = 1; $i <= $lastday; $i++){
             $dt = Carbon::create($checkdt->year,$checkdt->month,$i);
             array_push($date,$dt);
         }
 
-        // 従業員IDと従業員名の取得
-        $usercollection = collect(['userid','name','starttime','endtime','day','workingid']); //コレクションの定義
-        $employee = DB::select('select * from users');//従業員のデータ
-        
+        $usercollection = collect(['userid','name','starttime','endtime','day','workingid']);
+        $employee = User::where('deleted_at',null)->get();
+
         for($i = 0;$i < count($employee);$i++) {
+
             // 勤務時間をユーザーごとに取得
-            $working = DB::select('select * from working where year='.$checkdt->year.' and month='.$checkdt->month.' and userid='.$employee[$i]->id);
-            $starttime = array_column($working, 'starttime'); //勤務開始時間
-            $endtime = array_column($working, 'endtime'); //勤務終了時間
+            $working = Working::where('userid',$employee[$i]->id)->where('year',$checkdt->year)->where('month',$checkdt->month)->get()->toArray();
+
+            $starttime = array_column($working, 'starttime');
+            $endtime = array_column($working, 'endtime');
             $workingid = array_column($working, 'id');
-            $day = array_column($working, 'day'); //勤務日
+            $day = array_column($working, 'day');
 
             // 勤務時間をユーザーごとにコレクションに格納
             $data[$employee[$i]->id] = $usercollection->combine([$employee[$i]->id,$employee[$i]->name,$starttime,$endtime,$day,$workingid]);
@@ -69,14 +70,8 @@ class timelineController extends Controller
   
         return view('timeline.index',compact('data','lastday','date','weekday','checkdt','checkuserid','working'));
     }
-    //------------------------------------------------------/
-    // ------------- 勤務情報・日付情報の取得 -------------
-    //------------------------------------------------------/
+    
 
-
-    //------------------------------------------------------/
-    // ------ データベース操作(INSERT UPDATE DELETE) ------
-    //------------------------------------------------------/
     public function database(Request $request)
 	{   
         $requestdata = $request::all();
@@ -101,9 +96,5 @@ class timelineController extends Controller
         }
 	    return redirect($_SERVER['REQUEST_URI']);
     }
-    //------------------------------------------------------/
-    // ------ データベース操作(INSERT UPDATE DELETE) ------
-    //------------------------------------------------------/
-
 
 }
